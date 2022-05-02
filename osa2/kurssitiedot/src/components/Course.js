68e927b6c3a45a4230c73dded0bd789b4677c201
Jani Luukko <jani.luukko@reaktor.com>
2022-05-02 21:28:09 +0300

    2.2 kurssitiedot step7

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -12,12 +12,18 @@ const Content = ({ parts }) => {
   return parts.map(part => <Part key={part.id} part={part} />)
 }
 
+const Total = ({ parts }) => {
+  const numberOfExercises = parts.reduce((prev, cur) => prev + cur.exercises, 0)
+  return <p>{'total of ' + numberOfExercises + ' exercises'}</p>
+}
+
 const Course = ({ course }) => {
   const { name, parts, id } = course
   return (
     <>
     <Header name={name} />
     <Content parts={parts} />
+    <Total parts={parts} />
     </>
   )
 }
