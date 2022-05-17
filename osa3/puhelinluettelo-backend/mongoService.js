39ff05f96f05d87657b138c82d5636ddfc1229ab
Jani Luukko <jani.luukko@reaktor.com>
2022-05-17 23:48:03 +0300

    3.20 done

--- osa3/puhelinluettelo-backend/mongoService.js
+++ osa3/puhelinluettelo-backend/mongoService.js
@@ -4,6 +4,24 @@ const password = process.env.MONGODB_PASSWORD
 
 mongoose.connect(`mongodb+srv://phonebook:${password}@cluster0.qhji7.mongodb.net/?retryWrites=true&w=majority`);
 
-const Person = mongoose.model('Person', { id: mongoose.ObjectId, name: { type: String, minLength: 3}, number: String });
+const personProps = {
+  id: mongoose.ObjectId,
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'Person name required']
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: v => /^\d{2,3}-\d*$/.test(v),
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Person phone number required']
+  }
+}
+
+const Person = mongoose.model('Person', personProps);
 
 module.exports = { Person }
