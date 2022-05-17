49571fb3ec54504a92a512dd6df31d0ec3d7dc7d
Jani Luukko <jani.luukko@reaktor.com>
2022-05-17 23:57:19 +0300

    3.22 done

--- osa3/puhelinluettelo-backend/app.js
+++ osa3/puhelinluettelo-backend/app.js
@@ -1,51 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
-const isObject = require('lodash.isobject')
+const isObject = require('lodash.isobject');
 const cors = require('cors');
-let persons = require('./persons.json')
+const { isValidObjectId } = require('mongoose');
 
 const { Person } = require('./mongoService');
-const isValidObjectId = require('mongoose').isValidObjectId
-const res = require('express/lib/response');
 
 const app = express();
 
-morgan.token('body', (req, res) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body));
 
-const morganInstance = morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    tokens['body'](req,res)
-  ].join(' ')
-})
+const morganInstance = morgan((tokens, req, res) => [
+  tokens.method(req, res),
+  tokens.url(req, res),
+  tokens.status(req, res),
+  tokens.res(req, res, 'content-length'), '-',
+  tokens['response-time'](req, res), 'ms',
+  tokens.body(req, res),
+].join(' '));
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.use(morganInstance)
+app.use(express.json());
+app.use(morganInstance);
 
-app.use(express.static('frontend'))
+app.use(express.static('frontend'));
 
 // can't bother fixing this in frontend so
-const fixId = person => ({
+const fixId = (person) => ({
+  // eslint-disable-next-line no-underscore-dangle
   id: person._id,
   name: person.name,
-  number: person.number
-})
+  number: person.number,
+});
 
 const personsHandler = (_, res, next) => {
-  Person.find({}).then(persons => {
+  Person.find({}).then((persons) => {
     res.status(200).json(persons.map(fixId));
-  }).catch(err => next(err))
-}
+  }).catch((err) => next(err));
+};
 
 const infoHandler = (_, res) => {
   Person.count()
-    .then(count => {
+    .then((count) => {
       const html = `
       <html>
       <head>
@@ -55,110 +52,109 @@ const infoHandler = (_, res) => {
         <p>Phonebook has info for ${count} people</p>
         <p>${new Date()}</p>
       </body>
-    `
-    res.status(200).send(html.trim());
-    })
-}
+    `;
+      res.status(200).send(html.trim());
+    });
+};
 
 const singlePersonHandler = (req, res) => {
   if (!isValidObjectId(req.params.id)) {
-    res.status(500).json('Not a valid id')
-    return
+    res.status(500).json('Not a valid id');
+    return;
   }
   Person.findById(req.params.id)
-    .then(person => {
+    .then((person) => {
       if (person) {
-        res.status(200).send(fixId(person))
+        res.status(200).send(fixId(person));
       } else {
-        res.sendStatus(404)
+        res.sendStatus(404);
       }
-    })
-}
+    });
+};
 
 const deletePersonHandler = (req, res, next) => {
   if (!isValidObjectId(req.params.id)) {
-    res.status(500).json('Not a valid id')
-    return
+    res.status(500).json('Not a valid id');
+    return;
   }
   Person.findByIdAndRemove(req.params.id).then(() => {
-    res.sendStatus(204)
-  }).catch(err => next(err))
-}
+    res.sendStatus(204);
+  }).catch((err) => next(err));
+};
 
 const updatePersonHandler = (req, res, next) => {
   if (!isValidObjectId(req.params.id)) {
-    res.status(500).json('Not a valid id')
-    return
+    res.status(500).json('Not a valid id');
+    return;
   }
-  const { id } = req.params
-  const { name, number } = req.body
+  const { id } = req.params;
+  const { name, number } = req.body;
   Person.findByIdAndUpdate(id, { name, number })
-    .then(person => {
-      res.status(204).json(fixId(person))
-    }).catch(err => next(err))
-}
+    .then((person) => {
+      res.status(204).json(fixId(person));
+    }).catch((err) => next(err));
+};
 
 const insertPersonHandler = (req, res, next) => {
-  const mandatoryFields = ['name', 'number']
+  const mandatoryFields = ['name', 'number'];
   if (!isObject(req.body)) {
-    res.status(400).json({ error: 'Body data must be an JSON object'})
-    return
+    res.status(400).json({ error: 'Body data must be an JSON object' });
+    return;
   }
 
-  const fieldExist =
-    Object.fromEntries(
-      mandatoryFields
-      .map(field => [field, Object.keys(req.body).includes(field)])
-    )
-  const fieldsExist =
-    Object.values(fieldExist)
-      .every(bool => bool === true)
+  const fieldExist = Object.fromEntries(
+    mandatoryFields
+      .map((field) => [field, Object.keys(req.body).includes(field)]),
+  );
+  const fieldsExist = Object.values(fieldExist)
+    .every((bool) => bool === true);
   if (!fieldsExist) {
-    const fieldsMissing =
-      Object.entries(fieldExist)
-        .filter(([_, exists]) => exists === false)
-        .map(([field,]) => field)
-    const fieldWord = fieldsMissing.length > 1 ? 'fields' : 'field'
-    res.status(400).json({ error: `Following ${fieldWord} missing: ${fieldsMissing.join(', ')}`})
-    return
+    const fieldsMissing = Object.entries(fieldExist)
+      .filter(([, exists]) => exists === false)
+      .map(([field]) => field);
+    const fieldWord = fieldsMissing.length > 1 ? 'fields' : 'field';
+    res.status(400).json({ error: `Following ${fieldWord} missing: ${fieldsMissing.join(', ')}` });
+    return;
   }
 
   const newPerson = new Person({
     name: req.body.name,
-    number: req.body.number
-  })
-  newPerson.save().then(dbRes => {
-    res.status(201).json(dbRes)
-  }).catch(err => next(err))
-}
+    number: req.body.number,
+  });
+  newPerson.save().then((dbRes) => {
+    res.status(201).json(dbRes);
+  }).catch((err) => next(err));
+};
 
-app.get('/api/persons', personsHandler)
-app.post('/api/persons', insertPersonHandler)
-app.get('/api/persons/:id', singlePersonHandler)
-app.put('/api/persons/:id', updatePersonHandler)
-app.delete('/api/persons/:id', deletePersonHandler)
+app.get('/api/persons', personsHandler);
+app.post('/api/persons', insertPersonHandler);
+app.get('/api/persons/:id', singlePersonHandler);
+app.put('/api/persons/:id', updatePersonHandler);
+app.delete('/api/persons/:id', deletePersonHandler);
 
-app.get('/info', infoHandler)
+app.get('/info', infoHandler);
 
+// eslint-disable-next-line consistent-return
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  // eslint-disable-next-line no-console
+  console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' });
   }
 
-  next(error)
-}
+  next(error);
+};
 
-const myOwnErrorHandler = (error, request, response, next) => {
-  response.status(500).json({ error: error.message })
-}
+const myOwnErrorHandler = (error, request, response) => {
+  response.status(500).json({ error: error.message });
+};
 
-app.use(errorHandler)
-app.use(myOwnErrorHandler)
+app.use(errorHandler);
+app.use(myOwnErrorHandler);
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`Server running on port ${PORT}`);
+});
--- osa3/puhelinluettelo-backend/mongoService.js
+++ osa3/puhelinluettelo-backend/mongoService.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const password = process.env.MONGODB_PASSWORD
+const password = process.env.MONGODB_PASSWORD;
 
 mongoose.connect(`mongodb+srv://phonebook:${password}@cluster0.qhji7.mongodb.net/?retryWrites=true&w=majority`);
 
@@ -9,19 +9,19 @@ const personProps = {
   name: {
     type: String,
     minLength: 3,
-    required: [true, 'Person name required']
+    required: [true, 'Person name required'],
   },
   number: {
     type: String,
     minLength: 8,
     validate: {
-      validator: v => /^\d{2,3}-\d*$/.test(v),
-      message: props => `${props.value} is not a valid phone number!`
+      validator: (v) => /^\d{2,3}-\d*$/.test(v),
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
-    required: [true, 'Person phone number required']
-  }
-}
+    required: [true, 'Person phone number required'],
+  },
+};
 
 const Person = mongoose.model('Person', personProps);
 
-module.exports = { Person }
+module.exports = { Person };
