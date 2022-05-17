5ccff930862ff313189099a599f1973dc6480ad0
Jani Luukko <jani.luukko@reaktor.com>
2022-05-17 19:29:19 +0300

    3.2 puhelinluettelon backend step2 done

--- osa3/puhelinluettelo-backend/app.js
+++ osa3/puhelinluettelo-backend/app.js
@@ -7,7 +7,22 @@ const personsHandler = (req, res) => {
   res.status(200).json(persons);
 }
 
+const infoHandler = (req, res) => {
+  const html = `
+    <html>
+    <head>
+    <title>Phonebook info</title>
+    </html>
+    <body>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    </body>
+  `
+  res.status(200).send(html);
+}
+
 app.get('/api/persons', personsHandler)
+app.get('/info', infoHandler)
 
 const PORT = 3001
 app.listen(PORT, () => {
