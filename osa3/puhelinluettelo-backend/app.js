b57687cc9777e4abec870ef42497f078b0c5153d
Jani Luukko <jani.luukko@reaktor.com>
2022-05-17 20:37:50 +0300

    3.5 done

--- osa3/puhelinluettelo-backend/app.js
+++ osa3/puhelinluettelo-backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 let persons = require('./persons.json');
 
 const app = express();
+app.use(express.json())
 
 const personsHandler = (_, res) => {
   res.status(200).json(persons);
@@ -40,7 +41,31 @@ const deletePersonHandler = (req, res) => {
   res.sendStatus(204)
 }
 
+const insertPersonHandler = (req, res) => {
+  const mandatoryFields = ['name', 'number']
+  if (typeof req.body !== 'object') {
+    res.sendStatus(500)
+    return
+  }
+  const fieldsExist =
+    mandatoryFields
+      .map(field => Object.keys(req.body).includes(field))
+      .every(bool => bool === true)
+  if (!fieldsExist) {
+    res.status(400).json('Invalid fields')
+    return
+  }
+  const newPerson = {
+    id: Math.floor(Math.random() * 100),
+    name: req.body.name,
+    number: req.body.number
+  }
+  persons = [...persons, newPerson]
+  res.status(201).json(newPerson)
+}
+
 app.get('/api/persons', personsHandler)
+app.post('/api/persons', insertPersonHandler)
 app.get('/api/persons/:id', singlePersonHandler)
 app.delete('/api/persons/:id', deletePersonHandler)
 
