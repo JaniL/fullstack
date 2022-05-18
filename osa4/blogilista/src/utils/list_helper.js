d4dd9bb636455e69ef73a685203bf0feb0067ece
Jani Luukko <jani.luukko@reaktor.com>
2022-05-18 20:35:11 +0300

    4.5 done

--- osa4/blogilista/src/utils/list_helper.js
+++ osa4/blogilista/src/utils/list_helper.js
@@ -5,7 +5,12 @@ const totalLikes = blogs =>
     .map(blog => blog.likes)
     .reduce((prev, cur) => prev + cur, 0)
 
+const favoriteBlog = blogs =>
+  blogs
+    .reduce((prev, cur) => prev === null || cur.likes > prev.likes ? cur : prev, null)
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
--- osa4/blogilista/src/utils/list_helper.test.js
+++ osa4/blogilista/src/utils/list_helper.test.js
@@ -40,3 +40,30 @@ describe('total likes', () => {
     expect(result).toBe(8000)
   })
 })
+
+describe('favorite blog', () => {
+  test('is not found when the list is empty', () => {
+    const blogs = []
+
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toBe(null)
+  })
+
+  test('when list has only one blog equals that blog', () => {
+    const blogs = [
+      exampleBlogEntry
+    ]
+
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual(exampleBlogEntry)
+  })
+
+  test('is the blog with largest number of likes', () => {
+    const largestNumber = 9001
+    const groupOfLikeNumbers = [1000, largestNumber, 2000, 5000]
+    const blogs = groupOfLikeNumbers.map(num => ({...exampleBlogEntry, likes: num }))
+
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result).toEqual({...exampleBlogEntry, likes: largestNumber })
+  })
+})
